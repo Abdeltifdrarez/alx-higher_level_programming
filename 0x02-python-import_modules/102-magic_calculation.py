f4a24fedf4efe74895b0d0e4b5b2ad5d9f7bab53
abdeltif <drarezabdeltf@gmail.com>
2023-12-01 03:21:37 -0800

    fixed magic calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,3 +1,12 @@
-#!/usr/bin/python3
 def magic_calculation(a, b):
-	tup = ('add','sub')
+    add, sub = magic_calculation_102.add, magic_calculation_102.sub
+
+    if a < b:
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+        return c
+
+    return sub(a, b)
