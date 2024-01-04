9300461855fbd3b05eb05438573c6cf208c7b0fe
abdeltif <drarezabdeltf@gmail.com>
2024-01-04 00:16:18 -0800

    python-test-driven

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lazy_matrix function"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Function that prints a text with 2 new lines after each of
+    these characters: ., ? and :.
+
+    Returns :
+        Nothing
+
+    Raises :
+        TypeError in case the text is not string
+    """
